de9684153985c1434aef311972e33c691ebfa0f5
Izangildev <izangf2004@gmail.com>
2025-12-11 22:06:18 +0100

    Reto 3 - Nuevos microservicios y tests

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -35,5 +35,45 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "8", "ERROR SQRT"
         )
 
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/4/1"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "3", "ERROR ADD"
+        )
+        
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "9", "ERROR ADD"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/12/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "4.0", "ERROR ADD"
+        ) 
+    
+    def test_api_divide_by_zero(self):
+        url = f"{BASE_URL}/calc/divide/12/0"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, 406, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "ERROR DIVIDE /0", "El mensaje de error por división entre 0 no coincide"
+        ) 
+
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
